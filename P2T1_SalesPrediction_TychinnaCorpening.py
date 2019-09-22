1c491e568e9f3844dc79f6cdab45e177ec5275ce
Tychcorp <55641906+Tychcorp@users.noreply.github.com>
2019-09-21 21:58:12 -0400

    Add files via upload
    Second Submission

---
+++ P2T1_SalesPrediction_TychinnaCorpening.py
@@ -0,0 +1,18 @@
+# Get the projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+print('September 9,2019')
+print ('CTI-110 P2T1 - Sales Prediction')
+print ('Tychinna Corpening')
+
+
+#Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit,',.2f'))
+
+
+
+
+
