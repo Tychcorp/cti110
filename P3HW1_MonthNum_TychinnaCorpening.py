7b97d4e2b6e767296a6740195b57d33281d8cd21
Tychcorp <55641906+Tychcorp@users.noreply.github.com>
2019-10-14 20:48:07 -0400

    Add files via upload

---
+++ P3HW1_MonthNum_TychinnaCorpening.py
@@ -0,0 +1,50 @@
+# CTI-110
+    # P3HW1 - Month number
+    # Tychinna Corpening
+    # 10/2/2019
+
+# Determine the month between the numbers of 1 to 12
+# Use the if-elif-else rules to determine the answer
+# Month is shown within the 1 to 12 numbers or there is an error
+
+# Have the user to enter the month's number 1 through 12.
+month = int(input('What is the month number?'))
+
+if month == 1:
+     print('month is January')
+elif month == 2:
+     print('month is Febuary')
+
+elif month == 3:
+     print('month is March')
+
+elif month == 4:
+     print('month is April')
+
+elif month == 5:
+     print('month is May')
+
+elif month == 6:
+     print('month is June')
+     
+elif month == 7:
+     print('month is July')
+
+elif month == 8:
+     print('month is August')
+
+elif month == 9:
+     print('month is September')
+
+elif month == 10:
+     print('month is October')
+
+elif month == 11:
+     print('month is November')
+
+elif month == 12:
+     print('month is December')
+     
+else:
+    print('You should have entered a number between 1 -12.')
+
